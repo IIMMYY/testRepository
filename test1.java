d61ddcaa1e07014a21cd69ca08a37f2cad2242ba
IMYIMYIMY <IMYBCN@yahoo.com>
2017-01-27 13:44:04 +0800

    "testeryi"

---
+++ test1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class test1 {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 8 * 10;
+        System.out.println("hhh " + j);
+    }
+}
